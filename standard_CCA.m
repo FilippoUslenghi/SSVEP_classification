39a1af1d266fe0eab64afa2ec180aabfdc725095
filippouslenghi <us98@hotmail.it>
2023-01-08 19:50:17 +0100

    created matrix of the signal and target

--- standard_CCA.m
+++ standard_CCA.m
@@ -4,30 +4,33 @@ clc;
 close all
 
 fs = 1000;
-data_dir = "data/6hz-01.h5";
-data = h5read(data_dir, "/20:15:12:22:81:60/raw/channel_4");
-data = cast(data, "double");
+dataDir = "data/6hz_01.h5";
+data = h5read(dataDir, "/20:15:12:22:81:60/raw/channel_4");
+data = cast(data, "double")';
 
 % Filtering the signal
 % data = bandpass(data, [2, 40], fs);
 
-n_windows = 3;
-windowTime = 24; % seconds
+%%
+% Creating the X matrix of signal for the CCA
+windowTime = 2; % seconds
 windowLength = windowTime*fs;
+zeroPadding = zeros(windowLength-mod(length(data), windowLength), 1);
+padded_data = cat(1, data, zeroPadding);
 
-windows = zeros(windowLength, n_windows);
-for ii = 1:n_windows
-    windows(:, ii) = data(windowLength*(ii-1)+1:windowLength*ii);
+X = reshape(padded_data, [], windowLength);
+
+% Creating the Y matrix of the target frequency for the CCA
+targetFreq = 5; %Hz
+n_harmonics = 8;
+Y = zeros(n_harmonics*2, windowLength);
+t = (0:windowLength-1)/fs;
+
+for ii = 1:n_harmonics
+    Y(2*(ii)-1, :) = sin(2*pi*targetFreq*ii*t);
+    Y(2*(ii), :) = cos(2*pi*targetFreq*ii*t);
 end
 
-%%er
-first_window = windows(:, 1);
 
-n_samples = 10;
-sampleLength = windowLength/n_samples;
-samples = reshape(first_window, [sampleLength, n_samples]);
-
-targetFreqMatrix = zeros(size(samples));
-for ii = 1:n_samples
-    targetFreqMatrix(:, ii) = ;
-end
+%%
+[A,B] = canoncorr(X, Y);
