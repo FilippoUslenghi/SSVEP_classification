3cf9d10923194f2e390a838edb544ff930cb7381
filippouslenghi <us98@hotmail.it>
2023-01-25 11:19:36 +0100

    renamed parameter in load_data function

--- load_data.m
+++ load_data.m
@@ -1,7 +1,7 @@
-function data = load_data(dataDir)
+function data = load_data(dataFile)
 % Loads the data from the h5 file of the dataset and converts it into
 % double values
 
-    data = h5read(dataDir, "/20:15:12:22:81:60/raw/channel_4");
+    data = h5read(dataFile, "/20:15:12:22:81:60/raw/channel_4");
     data = cast(data, "double")';
 end
